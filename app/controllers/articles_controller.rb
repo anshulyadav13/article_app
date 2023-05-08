639f0134e31cb3bcb8fbb95af0ef0e4dc0975f5c
Anshul Yadav <anshul.yadav@w3villa.com>
2023-05-08 15:54:59 +0530

    crud DONE

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description));
+        if @article.save
+            flash[:notice] = "Article  Saved !"
+            redirect_to @article;
+        else
+            render 'new'
+       end
+    end
+    def new
+        @article = Article.new
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if  @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "hey you just edited the article"
+            redirect_to @article 
+        else
+            render 'edit'
+        end
+
+     end
+     def destroy
+        Article.destroy(params[:id])
+         flash[:alert] = "article deleted"
+        redirect_to articles_path
+     end
+end
