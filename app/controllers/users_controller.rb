55ff5872cb25abfa197d61d74895e361b9872de6
Anshul Yadav <anshul.yadav@w3villa.com>
2023-05-10 11:05:37 +0530

    user edit added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
-         @user = User.new(params.require(:user).permit(:user_name,:email,:password))
+         @user = User.new(get_url_params)
          if @user.save
             flash[:notice] ="Sucessfully signed Up"
             redirect_to articles_path
@@ -12,4 +12,28 @@ class UsersController < ApplicationController
          end
 
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+         @user = User.find(params[:id])
+         if @user.update(get_url_params)
+            flash[:notice] ="Profile Successfully Updated"
+            redirect_to articles_path
+         else
+            render 'edit'
+         end
+
+
+         # if User.update(get_url_params)
+
+    end
+
+
+    private
+    def get_url_params
+        params.require(:user).permit(:user_name,:email,:password)
+     end
+
 end
